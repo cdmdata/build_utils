0f3690a6767c87808c2eb51164109c6822287e34
raf1hh <rszuminski@kbb.com>
2013-02-11 15:27:00 -0800

    Fixed bug related to formating cache partition. Added params to create_recovery_ramdisk script

--- create_recovery_ramdisk.sh
+++ create_recovery_ramdisk.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# $1: target out
+
+if [ $# -ne 1 ]; then
+    echo "Error: wrong number of arguments in cmd: $0 $* "
+    return
+fi
 echo "MAKE SURE Android build/envsetup.sh has been run in this terminal and that you are running it from the root of the source tree!"
 echo "Create recovery ramdisk?"
 read -r -p "Are you sure you? [Y/n] " response
@@ -5,9 +13,9 @@ if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
 croot
 echo "Copy files into recovery root..."
-cp out/target/product/imx53_nitrogenk/system/bin/busybox out/target/product/imx53_nitrogenk/recovery/root/system/bin/
-cp out/target/product/imx53_nitrogenk/system/bin/recovery out/target/product/imx53_nitrogenk/recovery/root/sbin/
-cd out/target/product/imx53_nitrogenk/recovery/root
+cp $1/system/bin/busybox out/target/product/imx53_nitrogenk/recovery/root/system/bin/
+cp $1/system/bin/recovery out/target/product/imx53_nitrogenk/recovery/root/sbin/
+cd $1/recovery/root
 find . -print |cpio -H newc -o |gzip -9 > ../../recovery.ramdisk.cpio.gz
 mkimage -A arm -O linux -T ramdisk -n "Recovery Ram Disk" -d ../../recovery.ramdisk.cpio.gz ../../initrd_recovery.u-boot
 cd ../..
--- setupSDcard.sh
+++ setupSDcard.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 DRIVE=$1
-for n in ${DRIVE}* ; do umount $n ; done
-echo "About to delete from $@....";
+SOURCEDIR=$2
+
+
+if [ $# -ne 2 ]; then
+    echo "Error: wrong number of arguments in cmd: $0 $* "
+    return
+fi
+
+echo "About to wipe $DRIVE and copy from $SOURCEDIR...";
 read -r -p "Are you sure you? [Y/n] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]] 
 then
@@ -52,88 +59,21 @@ EOF
 echo "Creating New partitions...Done";
 echo "Formating partitions...";
 
-sudo mkfs.vfat -n BOOT ${DRIVE}1
-sudo mkfs.ext4 -L SYSTEM ${DRIVE}2
 sudo mkfs.ext4 -L CACHE ${DRIVE}5
-sudo mkfs.ext4 -L DATA ${DRIVE}6
-sudo mkfs.vfat -n RECOVERY ${DRIVE}7
 sudo mkfs.vfat -F 32 -n MEDIA ${DRIVE}4
 
-echo "Formating partitions...Done";
-echo "Drive Mount...";
-sudo mkdir /media/BOOT
-sudo mkdir /media/SYSTEM
-sudo mkdir /media/CACHE
-sudo mkdir /media/DATA
-sudo mkdir /media/RECOVERY
-sudo mkdir /media/MEDIA
+echo "Reimaging partitions...";
+echo "Restoring boot..."
+IMAGE_FILE=${SOURCEDIR}/fatboot.img
+sudo dd if=$IMAGE_FILE of=${DRIVE}1 bs=4096
 
-sudo mount ${DRIVE}1 /media/BOOT
-sudo mount ${DRIVE}2 /media/SYSTEM
-sudo mount ${DRIVE}5 /media/CACHE
-sudo mount ${DRIVE}6 /media/DATA
-sudo mount ${DRIVE}7 /media/RECOVERY
-sudo mount ${DRIVE}4 /media/MEDIA
-echo "Drive Mount...Done";
+echo "Restoring system...";
+IMAGE_FILE=${SOURCEDIR}/system.img
+sudo dd if=$IMAGE_FILE of=${DRIVE}2 bs=4096
 
-echo "Copy SYSTEM ...";
-sudo cp -ravf ../out/target/product/imx53_nitrogenk/system/* /media/SYSTEM/
-####sudo cp -ravf ../root/* /media/ROOT/
-echo "Copy DATA ...";
-sudo cp -ravf ../out/target/product/imx53_nitrogenk/data/* /media/DATA/
-echo "Copy RECOVERY ...";
-sudo cp -ravf ../out/target/product/imx53_nitrogenk/recovery/* /media/RECOVERY/
-echo "Copy KERNEL ...";
-sudo mkimage -A arm -O linux -T ramdisk -n "Initial Ram Disk" -d ../out/target/product/imx53_nitrogenk/ramdisk.img ../out/target/product/imx53_nitrogenk/initrd.u-boot
-sudo cp ../out/target/product/imx53_nitrogenk/initrd.u-boot /media/BOOT
-sudo cp ../nitrogen53_bootscript /media/BOOT
-
-sudo cp ../kernel_imx/arch/arm/boot/uImage /media/BOOT/uImage53
-sudo mkdir /media/BOOT/lib/
-sudo mkdir /media/BOOT/lib/modules/
-sudo cp -ravf ../kernel_imx/drivers/media/video/mxc/capture/*.ko /media/BOOT/lib/modules/
-sudo cp -ravf ../kernel_imx/drivers/i2c/xrp6840.ko /media/BOOT/lib/modules/
-
-#echo "Copy wireless drivers"
-sudo cp -ravf ../compat-wireless-2011-08-08/drivers/net/wireless/wl12xx/wl12xx.ko /media/BOOT/lib/modules/
-sudo cp -ravf ../compat-wireless-2011-08-08/drivers/net/wireless/wl12xx/wl12xx_sdio.ko /media/BOOT/lib/modules/
-sudo cp -ravf ../compat-wireless-2011-08-08/net/mac80211/mac80211.ko /media/BOOT/lib/modules/
-sudo cp -ravf ../compat-wireless-2011-08-08/net/wireless/cfg80211.ko /media/BOOT/lib/modules/
-sudo cp -ravf ../compat-wireless-2011-08-08/compat/compat.ko /media/BOOT/lib/modules/
-cd ..
-sudo sync
-echo "done"
-
-echo "set permissions"
-sudo chmod 777 -R /media/BOOT/
-sudo chmod 777 -R /media/SYSTEM/
-sudo chmod 777 -R /media/CACHE/
-sudo chmod 777 -R /media/DATA/
-sudo chmod 777 -R /media/RECOVERY/
-sudo chmod 777 -R /media/MEDIA/
-echo "set permissions...Done"
-echo "Umount..."
-sudo umount /media/BOOT
-sudo umount /media/SYSTEM
-sudo umount /media/CACHE
-sudo umount /media/DATA
-sudo umount /media/RECOVERY
-sudo umount /media/MEDIA
-for n in ${DRIVE}* ; do umount $n ; done
-echo "Umount...Done"
-echo "Remove Folder..."
-sudo rm -rf /media/SYSTEM/*
-sudo rm -rf /media/DATA/*
-sudo rm -rf /media/RECOVERY/*
-sudo rm -rf /media/CACHE/*
-
-sudo rmdir -v /media/BOOT
-sudo rmdir -v /media/SYSTEM
-sudo rmdir -v /media/CACHE
-sudo rmdir -v /media/DATA
-sudo rmdir -v /media/RECOVERY
-sudo rmdir -v /media/MEDIA
-echo "Remove Folder...Done"
+echo "Restoring userdata...";
+IMAGE_FILE=${SOURCEDIR}/userdata.img
+sudo dd if=$IMAGE_FILE of=${DRIVE}5 bs=4096
 fi
 
 
